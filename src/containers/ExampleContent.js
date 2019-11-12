f0b3623414e2b27c24201639b53596bf664bf1f1
Sam Jespersen <heyheysamj@gmail.com>
2019-11-12 13:07:25 -0800

    example content formatted somewhat okay

--- src/containers/ExampleContent.js
+++ src/containers/ExampleContent.js
@@ -3,26 +3,38 @@ import { useExampleContent } from './use-example-content';
 import Options from '../components/Options';
 import MazeDisplay from '../components/MazeDisplay';
 import styles from './ExampleContent.css';
+import tableStyles from '../components/Content.css';
 
 export default function ExampleContent() {
   const { data, apiKey, maze, size, cellShape, algorithm, handleChange, handleSubmit, handleToggleSolution } = useExampleContent();
-  
+
   return (
-    <div className={styles.ExampleContent}>
-      <Options
-        apiKey={apiKey}
-        size={size}
-        cellShape={cellShape}
-        algorithm={algorithm}
-        handleSubmit={handleSubmit}
-        handleChange={handleChange} 
-      />
-      {data &&
-        <MazeDisplay  
-          maze={maze}
-          handleToggleSolution={handleToggleSolution}
-        />
-      }
-    </div>
+    <table className={tableStyles.Content}>
+      <tbody>
+        <tr>
+          <th>GET MAZE EXAMPLE</th>
+        </tr>
+        <tr>
+          <td>
+            <div className={styles.ExampleContent}>
+              <Options
+                apiKey={apiKey}
+                size={size}
+                cellShape={cellShape}
+                algorithm={algorithm}
+                handleSubmit={handleSubmit}
+                handleChange={handleChange}
+              />
+              {data &&
+                <MazeDisplay
+                  maze={maze}
+                  handleToggleSolution={handleToggleSolution}
+                />
+              }
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
